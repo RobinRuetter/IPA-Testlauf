844f33c6de9b966616442e187ddb30eb148d4e1b
Robin Rütter <robin.ruetter@gmail.com>
2024-06-18 14:32:54 +0200

    erstelle tabellen

---
+++ ipa.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `ticket` (
+  `ticketID` int(11) NOT NULL,
+  `titel` varchar(255) NOT NULL,
+  `gueltigVon` datetime DEFAULT NULL,
+  `gueltigBis` datetime DEFAULT NULL,
+  `erstelltam` datetime DEFAULT NULL,
+  `nutzerid` int(10) UNSIGNED NOT NULL,
+  `link` varchar(50) DEFAULT NULL,
+  `datei` LONGBLOB DEFAULT NULL
+)
+CREATE TABLE `users` (
+  `nutzerid` int(11) NOT NULL,
+  `Benutzername` varchar(20) NOT NULL,
+  `Passwort` varchar(255) NOT NULL
+);
