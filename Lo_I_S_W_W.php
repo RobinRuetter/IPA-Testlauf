c6d292989463fb276a61b04230b24076514c79df
Robin Rütter <robin.ruetter@gmail.com>
2024-06-19 14:40:36 +0200

    Update Lo_I_S_W_W.php

--- Lo_I_S_W_W.php
+++ Lo_I_S_W_W.php
@@ -5,7 +5,7 @@ echo "Sie werden in 5 Sekunden weitergeleitet";
 echo "<br>";
 echo "geniesen Sie ein Bild von einem hübschen Otter während wir Sie abmelden";
 echo "<br>";
-echo "<img src=\"https://th.bing.com/th?id=ABTE090A2D04140E39D6FAE428D9A0661D58E65496A87392F02AA6B89CD7E92AA71&w=608&h=200&c=2&rs=1&o=6&dpr=1.3&pid=SANGAM\" alt=\"error 404 Sie sind nicht mit dem Internet verbunden\">";
+echo "<img src='https://upload.wikimedia.org/wikipedia/commons/e/ee/Sea_otter.jpg' alt='Otter'>";
 //delete session
 session_start();
 session_destroy();
